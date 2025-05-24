43e1fb86faaa17bd6e940c03fbd2f3d2f414e906
Sachithra_Manamperi <61633493+SachPrecious@users.noreply.github.com>
2025-05-24 12:57:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,3 @@ variable "api_gateway_name" {
   description = "The name of the API Gateway"
   type        = string
 }
-variable "lambda_invoke_arn" {
-  description = "The ARN of the Lambda function to be invoked"
-  type        = string
-}
